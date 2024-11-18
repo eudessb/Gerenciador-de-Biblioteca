c3666d42ea01aa5a5b06f47c89fba152ae9d7b21
eudessb <eudessilvabezerra@gmail.com>
2024-11-18 14:16:22 -0300

    NEW VERSION

--- functions/functions.h
+++ functions/functions.h
@@ -11,7 +11,6 @@ void inserir_livro(No** raiz, Livro livro);
 void buscar_por_genero(No* raiz, char genero[]);
 
 // Função para Carregar Livros do Arquivo CSV
-
 No* carregar_livros(char* nome_arquivo, No* raiz);
 
 //Função para Exibir a Árvore

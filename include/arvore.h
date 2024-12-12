93305a6d3444fa0257f38df7384817dde181c89a
eudessb <eudessilvabezerra985@gmail.com>
2024-12-12 08:52:30 -0300

    include

--- include/arvore.h
+++ include/arvore.h
@@ -1,14 +1,17 @@
+/**
+ *Cabeçalho com a declaração das funções implementadas no projeto. 
+ */
 #ifndef ARVORE_H
 #define ARVORE_H
 
 typedef struct Livro
 {
     int codigo;
-    char titulo[100];
-    char autor[100];
-    char genero[50];
+    char titulo[99];
+    char autor[99];
+    char genero[99];
     int ano;
-    char editora[100];
+    char editora[99];
     int numero_paginas;
 } Livro;
 
@@ -27,12 +30,14 @@ void inserir_livro(No **raiz, Livro livro);
 
 void buscar_por_genero(No *raiz, char *genero);
 
+void inserir_manualmente(No **raiz);
+
 No *carregar_livros(char *nome_arquivo, No *raiz);
 
 void exibir_arvore(No *raiz);
 
 void liberar_arvore(No *raiz);
 
-int arv_vazia(No *raiz);
+void exibir_menu();
 
 #endif
